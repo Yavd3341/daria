c6af64a8f8ad381bd2494acd529cf67a5036f1e8
Illia Yavdoshchuk <yavd3341@gmail.com>
2023-11-12 16:04:53 +0200

    Storage support for login manager

--- plugins/login-mgr/plugin.js
+++ plugins/login-mgr/plugin.js
@@ -4,16 +4,22 @@
 
 const Router = require("@koa/router");
 
-function makeEndpoints() {
+function makeEndpoints(storage) {
   const router = new Router();
 
   router.prefix("/auth")
 
   router.post("/", ctx => {
-    if (ctx.json.user == "admin") {
-      if (ctx.json.pass == "admin") {
+    if (ctx.json.user === storage.username) {
+      if (checkPassowrd(ctx.json.pass, storage)) {
         ctx.status = 200;
-        ctx.cookies.set("daria", "daria");
+        dropCookie(ctx.cookies.get("daria"));
+
+        const cookie = getNewCookie();
+        ctx.cookies.set("daria", cookie);
+        
+        storage.cookies.push(cookie);
+        storage.save();
       }
       else {
         ctx.status = 401;
@@ -31,15 +37,16 @@ function makeEndpoints() {
 
   router.post("/logout", ctx => {
     ctx.status = 200;
+    dropCookie(ctx.cookies.get("daria"));
     ctx.cookies.set("daria", undefined);
   });
 
   return router;
 }
 
-function makeSessionGuard() {
+function makeSessionGuard(storage) {
   return (ctx, next) => {
-    if (ctx.cookies.get("daria") == "daria" 
+    if (storage.cookies.includes(ctx.cookies.get("daria"))
     || (ctx.method == "POST" && ctx.URL.pathname == "/auth"))
       return next();
     else
@@ -47,9 +54,47 @@ function makeSessionGuard() {
   }
 }
 
+function loadStorage(pluginManager) {
+  const defaultStorage = {
+    username: "admin",
+    password: "admin",
+    plain: true,
+    cookies: []
+  }
+
+  const storageManager = pluginManager.getPlugin("storage-mgr");
+  let storage = storageManager.getStorage("auth", defaultStorage);
+  storage.load();
+  storage.save();
+
+  return storage;
+}
+
+function checkPassowrd(pass, storage) {
+  if (storage.plain)
+    return pass === storage.password;
+  else
+    false;
+}
+
+function getNewCookie() {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz123456789";
+  const length = 32;
+  let cookie = '';
+  for (let i = 0; i < length; i++)
+    cookie += chars[Math.floor(Math.random() * chars.length)];
+  return cookie;
+}
+
+async function dropCookie(cookie) {
+  if (storage.cookies.includes(cookie))
+    storage.cookies.splice(storage.cookies.indexOf(cookie));
+}
+
 module.exports = {
   init(ctx) {
-    ctx.router = makeEndpoints();
-    ctx.koa.use(makeSessionGuard());
+    const storage = loadStorage(ctx.pluginManager);
+    ctx.router = makeEndpoints(storage);
+    ctx.koa.use(makeSessionGuard(storage));
   }
 }
