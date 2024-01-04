3758637bc288718e685bcd00bfe022f4e41f4921
Illia Yavdoshchuk <yavd3341@gmail.com>
2024-01-05 01:29:20 +0200

    Improve error visuals on login page

--- ui/scripts/login.js
+++ ui/scripts/login.js
@@ -15,25 +15,31 @@ document.addEventListener("DOMContentLoaded", _ => {
   let loginBtn = loginForm.getElementsByTagName("button")[0];
 
   loginForm.onsubmit = event => {
+    loginUser.classList.remove("error");
+    loginPass.classList.remove("error");
+    loginBtn.classList.remove("error");
+
+    loginBtn.innerText = "Logging in...";
+
     postAjax("/api/auth", {
       user: loginUser.value,
       pass: loginPass.value,
     }, xhr => {
       if (xhr.readyState == 4) {
-        if (xhr.status == 200) {
-          loginBtn.classList.remove("error");
-          loginBtn.innerText = "Logging in...";
+        if (xhr.status == 200)
           location.href = "/";
-        } else {
+        else {
           loginBtn.classList.add("error");
           if (xhr.status == 401) {
             const response = xhr.response;
             switch (response.result) {
               case "err_user":
                 loginBtn.innerText = "Invalid username";
+                loginUser.classList.add("error");
                 break;
               case "err_pass":
                 loginBtn.innerText = "Invalid password";
+                loginPass.classList.add("error");
                 break;
               default:
                 loginBtn.innerText = "Forbidden";
