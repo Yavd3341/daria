e936be534bb2e1ba0e2d06d78ed9aed810dfd084
Illia Yavdoshchuk <yavd3341@gmail.com>
2024-02-20 17:12:48 +0200

    Add settings page code

--- plugins/norma-plus/resources/settings/loader.js
+++ plugins/norma-plus/resources/settings/loader.js
@@ -1,36 +1,143 @@
 daria.builders["settings"] = (fragment, ctx) => {
-  let form = fragment.getElementById("accountEdit");
+  // Settings
+  {
+    const form = fragment.getElementById("settingsForm");
 
-  let inputAccount = fragment.getElementById("account");
-  let inputUsername = fragment.getElementById("user");
-  let inputPassword = fragment.getElementById("newPass");
-  
-  let submit = fragment.getElementById("submit");
+    const inputHost = fragment.getElementById("host");
+    inputHost.value = ctx.database.host || "";
 
-  form.onsubmit = event => {
-    submit.classList.remove("error");
+    const inputPort = fragment.getElementById("port");
+    inputPort.value = ctx.database.port || "";
 
-    submit.innerText = "Saving...";
+    const inputDatabase = fragment.getElementById("database");
+    inputDatabase.value = ctx.database.database || "";
 
-    postAjax("/api/norma-plus/settings", {
-      account: inputAccount.value,
-      username: inputUsername.value,
-      password: inputPassword.value
-    }, xhr => {
-      if (xhr.readyState == 4) {
-        if (xhr.status == 200)
-          buildCards(false);
-        else {
-          submit.classList.add("error");
-          submit.innerText = "Failed to apply";
-        }
+    function getUserFields(user) {
+      const userSection = fragment.getElementById(user + "User");
+      const inputs = userSection.getElementsByTagName("input");
+
+      const inputUsername = inputs[0];
+      inputUsername.value = ctx.users[user] || "";
+
+      const inputPassword = inputs[1];
+      if (!inputUsername.value) {
+        inputPassword.required = true;
+        inputPassword.placeholder = inputPassword.placeholder.replace(" new", "");
       }
-    });
+      
+      return { inputUsername, inputPassword };
+    }
 
-    event.preventDefault();
-  };
+    const userLogin = getUserFields("login");
+    const userGatherer = getUserFields("gatherer");
+    const userUser = getUserFields("user");
 
-  // let refreshSession = fragment.getElementById("refreshSession");
-  // refreshSession.onclick = () => 
-  //   postAjax("/api/norma-plus/refresh-session", {});
+    const toggleUseInternalTimer = fragment.getElementById("useInternalTimer");
+    toggleUseInternalTimer.checked = ctx.useInternalTimer;
+
+    let submit = fragment.getElementById("submit");
+
+    form.onsubmit = event => {
+      submit.classList.remove("error");
+  
+      submit.innerText = "Saving...";
+  
+      postAjax("/api/norma-plus/settings", {
+        database: {
+          host: inputHost.value,
+          password: inputPort.value,
+          database: inputDatabase.value,
+        },
+        credentials: {
+          login: {
+            user: userLogin.inputUsername.value,
+            password: userLogin.inputPassword.value
+          },
+          gatherer: {
+            user: userGatherer.inputUsername.value,
+            password: userGatherer.inputPassword.value
+          },
+          user: {
+            user: userUser.inputUsername.value,
+            password: userUser.inputPassword.value
+          }
+        },
+        useInternalTimer: toggleUseInternalTimer.checked
+      }, xhr => {
+        if (xhr.readyState == 4) {
+          if (xhr.status == 200)
+            submit.innerText = "Save";
+          else {
+            submit.classList.add("error");
+            submit.innerText = "Failed to apply";
+          }
+        }
+      });
+  
+      event.preventDefault();
+    };
+  }
+
+  // Accounts
+  {
+    const rowTemplate = fragment.getElementById("row");
+    const accounts = fragment.getElementById("accounts");
+
+    function addRow(data, parent) {
+      const row = rowTemplate.content.cloneNode(true);
+
+      const accountCell = row.getElementById("account");
+      accountCell.innerText = data.account;
+      row.getElementById("name").innerText = data.name;
+      row.getElementById("address").innerText = data.address;
+
+      row.getElementById("delete").onclick = event => {
+        if (confirm("This will irreversibly delete all history associated with account!\n\nAre you sure?")) {
+          const row = accountCell.parentElement;
+          ajax("DELETE", "/api/norma-plus/account/" + data.account, {}, xhr => {
+            if (xhr.readyState == 4 && xhr.status == 200)
+              row.parentElement.removeChild(row);
+          });
+        }
+      };
+
+      parent.appendChild(row);
+    }
+
+    const form = fragment.getElementById("accountEdit");
+
+    const inputAccount = fragment.getElementById("account");
+    const inputUser = fragment.getElementById("user");
+    const inputPass = fragment.getElementById("pass");
+
+    let saveCreds = fragment.getElementById("saveCreds");
+
+    form.onsubmit = event => {
+      saveCreds.classList.remove("error");
+  
+      saveCreds.innerText = "Saving...";
+  
+      postAjax("/api/norma-plus/account", {
+        account: inputAccount.value,
+        username: inputUser.value,
+        password: inputPass.value
+      }, xhr => {
+        if (xhr.readyState == 4) {
+          if (xhr.status == 200)
+            saveCreds.innerText = "Save";
+          else {
+            saveCreds.classList.add("error");
+            saveCreds.innerText = "Failed to apply";
+          }
+        }
+      });
+  
+      event.preventDefault();
+    };
+
+    const tableFragment = document.createDocumentFragment();
+    for (const account of ctx.accounts)
+      addRow(account, tableFragment);
+    accounts.appendChild(tableFragment);
+  }
 };
