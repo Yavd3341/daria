9de5cc47b427e295484dfb542b93ec87f2f9e545
Illia Yavdoshchuk <yavd3341@gmail.com>
2024-05-23 07:32:25 +0300

    Add Utility Meters plugin

---
+++ plugins/utility-meters/api/db.js
@@ -0,0 +1,244 @@
+// Database users
+var admin, gatherer, guest;
+
+// External config and library
+var config;
+var postgres;
+
+// Internal variables
+var usesDefaultHost;
+var connected;
+
+function errorHandler(error) {
+  console.error("[Utility Meters] " + error);
+  return undefined;
+}
+
+function onDefaultHostChange(event) {
+  if (usesDefaultHost && event == "postgres-default-host-updated")
+    tryConnect();
+}
+
+async function tryConnect() {
+  function isValidUser(user) {
+    return user.user && user.password;
+  }
+
+  if (!config.database.database 
+   || !isValidUser(config.credentials.admin)
+   || !isValidUser(config.credentials.gatherer)
+   || !isValidUser(config.credentials.guest))
+    return false;
+
+  await Promise.allSettled([
+    admin?.end(),
+    gatherer?.end(),
+    guest?.end()
+  ]);
+
+  connected = false;
+
+  usesDefaultHost = !config.database.host || !config.database.port;
+  const db = usesDefaultHost
+    ? {
+        ...postgres.getDefaultHost(), 
+        database: config.database.database
+      }
+    : config.database;
+
+  admin = postgres.makePool(config.credentials.admin, db);
+  gatherer = postgres.makePool(config.credentials.gatherer, db);
+  guest = postgres.makePool(config.credentials.guest, db);
+
+  try {
+    await Promise.all([
+      admin.query("SELECT NOW()"),
+      gatherer.query("SELECT NOW()"),
+      guest.query("SELECT NOW()")
+    ]);
+    connected = true;
+  }
+  catch (error) {
+    errorHandler(error);
+  }
+
+  return connected;
+}
+
+module.exports = {
+  async init(pgLib, cfg, eventBus) {
+    config = cfg;
+    postgres = pgLib;
+
+    eventBus.registerListener(onDefaultHostChange);
+
+    await this.tryConnect();
+  },
+
+  tryConnect,
+  isConnected() {
+    return connected;
+  },
+
+  // Get info
+
+  async getMeters(id) {
+    return (id != undefined
+      ? guest?.query("SELECT * FROM meters WHERE id = $1", [id])
+      : guest?.query("SELECT * FROM meters"))
+        .then(response => response.rows).catch(errorHandler)
+  },
+
+  async getTariffs(id) {
+    return (id != undefined
+      ? guest?.query("SELECT * FROM tariffs WHERE id = $1", [id])
+      : guest?.query("SELECT * FROM tariffs"))
+        .then(response => response.rows).catch(errorHandler)
+  },
+
+  async getGroups(id) {
+    return (id != undefined
+      ? guest?.query("SELECT * FROM groups WHERE id = $1", [id])
+      : guest?.query("SELECT * FROM groups"))
+        .then(response => response.rows).catch(errorHandler)
+  },
+
+  async getMetersInGroup(groupId) {
+    return guest?.query("SELECT meters.* FROM meters LEFT JOIN group_links ON group_links.meter_id = id WHERE group_id = $1", [groupId])
+      .then(response => response.rows).catch(errorHandler)
+  },
+
+  // Edit info
+  
+  async insertMeter(comment, tariffId) {
+    return admin?.query("INSERT INTO meters(comment, tariff) VALUES ($1, $2) RETURNING ID", [comment, tariffId])
+      .then(response => response.rows[0].id).catch(errorHandler)
+  },
+  async updateMeter(id, comment, tariffId) {
+    let cols = [];
+    let data = [id];
+    let counter = 2;
+
+    if (comment != undefined) {
+        cols.push(`comment = $${counter++}`);
+        data.push(comment);
+    }
+    if (tariffId != undefined) {
+        cols.push(`tariff = $${counter++}`);
+        data.push(tariffId);
+    }
+
+    if (cols.length)
+      return admin.query(`UPDATE meters SET ${cols.join(',')} WHERE id = $1`, data);
+  },
+  async deleteMeter(id) {
+    return admin.query('DELETE FROM meters WHERE id = $1', [id]);
+  },
+  
+  async insertTariff(comment) {
+    return admin?.query("INSERT INTO tariffs(comment) VALUES ($1) RETURNING ID", [comment])
+      .then(response => response.rows[0].id).catch(errorHandler)
+  },
+  async updateTariff(id, comment) {
+    return admin.query("UPDATE tariffs SET comment = $2 WHERE id = $1", [id, comment])
+      .catch(errorHandler);
+  },
+  async deleteTariff(id) {
+      return admin.query('DELETE tariffs WHERE id = $1', [id]);
+  },
+  
+  async insertGroup(comment) {
+    return admin?.query("INSERT INTO groups(comment) VALUES ($1) RETURNING ID", [comment])
+      .then(response => response.rows[0].id).catch(errorHandler)
+  },
+  async updateGroup(id, comment) {
+    return admin.query("UPDATE groups SET comment = $2 WHERE id = $1", [id, comment])
+      .catch(errorHandler);
+  },
+  async deleteGroup(id) {
+      return admin.query('DELETE groups WHERE id = $1', [id]);
+  },
+  
+  async linkMeterToGroup(meterId, groupId) {
+    return admin?.query("INSERT INTO group_links(meter_id, group_id) VALUES ($1, $2)", [meterId, groupId])
+      .catch(errorHandler)
+  },
+  async unlinkMeterFromGroup(meterId, groupId) {
+    return admin.query("DELETE group_links WHERE meter_id = $1 AND group_id = $2", [meterId, groupId])
+      .catch(errorHandler);
+  },
+
+  // Get historic data
+
+  async getMeterLog(id, maxMonths) {
+    return (maxMonths
+      ? guest?.query("SELECT date, value FROM meter_log WHERE meter = $1 AND NOW() - date < $2", [id, maxMonths + " MONTHS"])
+      : guest?.query("SELECT date, value FROM meter_log WHERE meter = $1", [id]))
+        .then(response => response.rows).catch(errorHandler)
+  },
+
+  async getTariffHistory(id, maxMonths) {
+    return (maxMonths
+      ? guest?.query("SELECT date, value FROM tariff_history WHERE tariff = $1 AND NOW() - date < $2", [id, maxMonths + " MONTHS"])
+      : guest?.query("SELECT date, value FROM tariff_history WHERE tariff = $1", [id]))
+        .then(response => response.rows).catch(errorHandler)
+  },
+
+  // Upsert historic data
+
+  async upsertTariffValue(tariffId, value, date) {
+    return gatherer?.query("INSERT INTO tariff_history(tariff, value, date) VALUES ($1, $2, $3) ON CONFLICT DO UPDATE SET value = EXCLUDED.value",
+      [tariffId, value, date])
+        .catch(errorHandler)
+  },
+  async upsertMeterReading(meterId, reading, date) {
+    return gatherer?.query("INSERT INTO meter_log(meter, value, date) VALUES ($1, $2, $3) ON CONFLICT DO UPDATE SET value = EXCLUDED.value",
+      [meterId, reading, date])
+        .catch(errorHandler)
+  },
+
+  // Extra requests
+
+  async getCurrentMeterInfo(id, isGroupId = false) {
+    const sqlWhereId = id 
+      ? isGroupId
+        ? "WHERE meter = $1"
+        : "WHERE meter IN (SELECT meter_id FROM group_links WHERE group_id = $1)"
+      : ""
+    return guest?.query(`WITH log AS (SELECT DISTINCT ON (meter) meter, value, value - LAG(value, 1) OVER (PARTITION BY meter ORDER BY date) difference  FROM meter_log ORDER BY meter, date DESC), tariffs AS (SELECT DISTINCT (tariff) tariff, value, date FROM tariff_history ORDER BY date DESC) SELECT comment, log.value reading, difference, tariffs.value tariff, difference * tariffs.value cost FROM meters LEFT JOIN tariffs ON tariffs.tariff = meters.tariff LEFT JOIN log ON log.meter = meters.id ${sqlWhereId}`, id ? [id] : [])
+      .then(response => response.rows).catch(errorHandler)
+  },
+
+  async getCurrentTariff(id) {
+    const sqlWhereId = id ? "WHERE tariff = $1" : ""
+    return guest?.query(`WITH last_entry AS (SELECT DISTINCT (tariff) tariff, date, value FROM tariff_history ORDER BY date DESC) SELECT tariffs.*, date since, value FROM tariffs LEFT JOIN last_entry ON tariff = id ${sqlWhereId}`, id ? [id] : [])
+      .then(response => response.rows).catch(errorHandler)
+  },
+
+  async getMeterLogWithTariff(id, maxMonths) {
+    const sqlWhereMonths = maxMonths 
+      ? "WHERE NOW() - date < $2"
+      : ""
+    return guest?.query(`WITH log AS (SELECT date, value, value - LAG(value, 1) OVER (PARTITION BY meter ORDER BY date) difference, (SELECT value FROM tariff_history WHERE tariff_history.tariff = meters.tariff AND tariff_history.date < meter_log.date ORDER BY date DESC LIMIT 1) tariff FROM meter_log LEFT JOIN meters ON meters.id = meter_log.meter WHERE meter = $1 ORDER BY date DESC) SELECT date, value reading, difference, tariff, difference * tariff cost FROM log ${sqlWhereMonths}`, maxMonths ? [id, maxMonths + "MONTHS"] : [id])
+      .then(response => response.rows).catch(errorHandler)
+  },
+
+  async getMeterLogCostGraph(groupId, maxMonths) {
+    let data = []
+
+    let sqlWhereId = ""
+    if (groupId) {
+      data.push(groupId)
+      sqlWhereId = `WHERE meter IN (SELECT meter_id FROM group_links WHERE group_id = $${data.length})`
+    }
+
+    let sqlWhereMonths = ""
+    if (maxMonths) {
+      data.push(maxMonths + " MONTHS")
+      sqlWhereMonths = `WHERE NOW() - date < $${data.length})`
+    }
+
+    return guest?.query(`WITH log AS (SELECT DATE_TRUNC('month', date - '1 WEEK'::INTERVAL) date, (value - LAG(value, 1) OVER (PARTITION BY meter ORDER BY date)) * (SELECT value FROM tariff_history WHERE tariff_history.tariff = meters.tariff AND tariff_history.date < meter_log.date ORDER BY date DESC LIMIT 1) cost FROM meter_log LEFT JOIN meters ON meters.id = meter_log.meter ${sqlWhereId}) SELECT date, SUM(cost) cost FROM log GROUP BY date ${sqlWhereMonths} ORDER BY date DESC`, data)
+      .then(response => response.rows).catch(errorHandler)
+  },
+};
---
+++ plugins/utility-meters/api/index.js
@@ -0,0 +1,114 @@
+const db = require("./db");
+var config = undefined;
+
+const Router = require("@koa/router");
+
+function makeEndpoints() {
+  const router = new Router();
+
+  router.prefix("/utility-meters");
+
+  router.safeGet = (path, data) => router.get(path, async ctx => ctx.body = await data(ctx) || [])
+  router.safeGet = (path, data) => router.get(path, async ctx => ctx.body = await data(ctx) || [])
+
+  router.get("/meters", async ctx => ctx.body = await db.getMeters() || [])
+  router.post("/meters", async ctx => ctx.body = await db.insertMeter(ctx.json.comment, ctx.json.tariff) || [])
+  router.get("/meter/:id", async ctx => ctx.body = await db.getMeters(ctx.params.id) || [])
+  router.post("/meter/:id", async ctx => {
+    db.updateMeter(ctx.params.id, ctx.json.comment, ctx.json.tariff)
+    ctx.response = 200
+  })
+  router.delete("/meter/:id", async ctx => {
+    db.deleteMeter(ctx.params.id)
+    ctx.response = 200
+  })
+
+  router.get("/meter/:id/readings", async ctx => ctx.body = await db.getMeterLog(ctx.params.id, ctx.query.months) || [])
+  router.get("/meter/:id/readings/full", async ctx => ctx.body = await db.getMeterLogWithTariff(ctx.params.id, ctx.query.months) || [])
+  router.post("/meter/:id/readings", async ctx => {
+    await db.upsertMeterReading(ctx.params.id, ctx.json.reading, ctx.json.date)
+    ctx.response = 200
+  })
+
+  router.get("/tariffs", async ctx => ctx.body = await db.getTariffs() || [])
+  router.post("/tariffs", async ctx => ctx.body = await db.insertTariff(ctx.json.comment) || [])
+  router.get("/tariff/:id", async ctx => ctx.body = await db.getTariffs(ctx.params.id) || [])
+  router.post("/tariff/:id", async ctx => {
+    db.updateTariff(ctx.params.id, ctx.json.comment)
+    ctx.response = 200
+  })
+  router.delete("/tariff/:id", async ctx => {
+    db.deleteTariff(ctx.params.id)
+    ctx.response = 200
+  })
+
+  router.get("/tariff/:id/values", async ctx => ctx.body = await db.getTariffHistory(ctx.params.id, ctx.query.months) || [])
+  router.post("/tariff/:id/values", async ctx => {
+    await db.upsertTariffValue(ctx.params.id, ctx.json.reading, ctx.json.date)
+    ctx.response = 200
+  })
+  router.get("/tariff/:id/values/current", async ctx => ctx.body = await db.getCurrentTariff(ctx.params.id) || [])
+
+  router.get("/groups", async ctx => ctx.body = await db.getGroups() || [])
+  router.post("/groups", async ctx => ctx.body = await db.insertGroup(ctx.json.comment) || [])
+  router.get("/group/:id", async ctx => ctx.body = await db.getGroups(ctx.params.id) || [])
+  router.post("/group/:id", async ctx => {
+    db.updateGroup(ctx.params.id, ctx.json.comment)
+    ctx.response = 200
+  })
+  router.delete("/group/:id", async ctx => {
+    db.deleteGroup(ctx.params.id)
+    ctx.response = 200
+  })
+
+  router.get("/group/:id/meters", async ctx => ctx.body = await db.getMetersInGroup(ctx.params.id) || [])
+  router.post("/group/:id/meters/:meter", async ctx => {
+    await db.linkMeterToGroup(ctx.params.meter, ctx.params.id)
+    ctx.response = 200
+  })
+  router.delete("/group/:id/meters/:meter", async ctx => {
+    await db.unlinkMeterFromGroup(ctx.params.meter, ctx.params.id)
+    ctx.response = 200
+  })
+
+  router.get("/meters/status", async ctx => ctx.body = await db.getCurrentMeterInfo() || [])
+  router.get("/meter/:id/status", async ctx => ctx.body = await db.getCurrentMeterInfo(ctx.params.id, false) || [])
+  router.get("/group/:id/status", async ctx => ctx.body = await db.getCurrentMeterInfo(ctx.params.id, true) || [])
+
+  router.get("/meters/graph", async ctx => ctx.body = await db.getMeterLogCostGraph(undefined, ctx.query.months) || [])
+  router.get("/group/:id/graph", async ctx => ctx.body = await db.getMeterLogCostGraph(ctx.params.id, ctx.query.months) || [])
+
+  return router;
+}
+
+module.exports = {
+  init(ctx) {
+    ctx.router = makeEndpoints();
+
+    const defaultConfig = {
+      database: {},
+      credentials: {
+        admin: {
+          user: "admin",
+          password: "changeme"
+        },
+        gatherer: {
+          user: "gatherer",
+          password: "changeme"
+        },
+        guest: {
+          user: "guest",
+          password: "changeme"
+        }
+      }
+    };
+
+    const configManager = ctx.pluginManager.getPlugin("storage-mgr");
+    config = configManager.getStorage("utility-meters", defaultConfig);
+    config.load();
+
+    db.init(ctx.pluginManager.getPlugin("postgresql"), config, ctx.eventBus);
+
+    require("./ui")(ctx, config);
+  }
+};
